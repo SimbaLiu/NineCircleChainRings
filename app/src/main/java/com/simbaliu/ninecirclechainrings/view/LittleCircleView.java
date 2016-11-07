13d9730d6e74949f17c9df2e57e7d59ae9af81b0
SimbaLiu3 <446550266@qq.com>
2016-11-07 17:09:11 +0800

    [lx] 11/07 子View加到viewGroup

--- app/src/main/java/com/simbaliu/ninecirclechainrings/view/LittleCircleView.java
+++ app/src/main/java/com/simbaliu/ninecirclechainrings/view/LittleCircleView.java
@@ -1,6 +1,7 @@
 package com.simbaliu.ninecirclechainrings.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
@@ -12,6 +13,9 @@ import android.view.View;
 public class LittleCircleView extends View {
 
     Paint mPaint = new Paint();
+    Paint textPaint = new Paint();
+    private int x, y;
+    private int num;
 
     public LittleCircleView(Context context) {
         super(context);
@@ -24,10 +28,29 @@ public class LittleCircleView extends View {
     }
 
     private void initPaint() {
-        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mPaint.setColor(Color.RED);
-        mPaint.setStrokeWidth(5);
+        mPaint.setStrokeWidth(0);
         mPaint.setAntiAlias(true);
         mPaint.setFilterBitmap(true);
+
+        textPaint.setColor(Color.BLACK);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextSize(40);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        x = y = 500;
+        canvas.drawCircle(x, y, 30, mPaint);
+        canvas.drawText("" + num, x, y, textPaint);
+    }
+
+    public void setCoordinateAndNum(int x, int y, int num) {
+        this.x = x;
+        this.y = y;
+        this.num = num;
+    }
+
 }
